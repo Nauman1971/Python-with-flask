66171bdad8b4cf903a3b7230062d1865658a5939
awais-ak47 <mawais0047@gmail.com>
2019-12-14 22:35:53 +0500

    databse

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_bootstrap import Bootstrap
 from forms import LoginForm, SignupForm
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_fontawesome import FontAwesome
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 from io import BytesIO
 
 login_manager = LoginManager()
@@ -15,6 +18,12 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 bootstrap = Bootstrap(app)
 fa = FontAwesome(app)
+migrate = Migrate(compare_type=True)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 
 
 class User(UserMixin, db.Model):
@@ -141,4 +150,4 @@ def logout():
 
 
 if __file__ == "__main__":
-    app.run(debug=True)
+    manager.run(debug=True)
--- config.py
+++ config.py
@@ -1,23 +1,27 @@
 import os
 
 
-class Config:
-    project_dir = os.path.dirname(os.path.abspath(__file__))
-    database_file = "sqlite:///" + os.path.join(project_dir) + "\db" + "\database.db"
-    print(database_file)
-    os.environ['SQLALCHEMY_DATABASE_URI'] = database_file
-    os.environ['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-    os.environ['SECRET_KEY'] = 'thisissupposed'
-    os.environ['RECAPTCHA_USE_SSL'] = "False"
-    os.environ['RECAPTCHA_PUBLIC_KEY'] = 'public'
-    os.environ['RECAPTCHA_PRIVATE_KEY'] = 'private'
-    os.environ['BOOTSTRAP_SERVE_LOCAL'] = 'True'
+# class Config:
+    # project_dir = os.path.dirname(os.path.abspath(__file__))
+    # database_file = "sqlite:///" + os.path.join(project_dir) + "\db" + "\database.db"
+    # print(database_file)
+    # os.environ['SQLALCHEMY_DATABASE_URI'] = database_file
+    # os.environ['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+    # os.environ['SECRET_KEY'] = 'thisissupposed'
+    # os.environ['RECAPTCHA_USE_SSL'] = "False"
+    # os.environ['RECAPTCHA_PUBLIC_KEY'] = 'public'
+    # os.environ['RECAPTCHA_PRIVATE_KEY'] = 'private'
+    # os.environ['BOOTSTRAP_SERVE_LOCAL'] = 'True'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    # BOOTSTRAP_SERVE_LOCAL = os.environ.get('BOOTSTRAP_SERVE_LOCAL')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    # RECAPTCHA_USE_SSL = os.environ.get('RECAPTCHA_USE_SSL')
+    # RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    # RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
+    # RECAPTCHA_OPTIONS = os.environ.get('RECAPTCHA_OPTIONS')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
-    BOOTSTRAP_SERVE_LOCAL = os.environ.get('BOOTSTRAP_SERVE_LOCAL')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    RECAPTCHA_USE_SSL = os.environ.get('RECAPTCHA_USE_SSL')
-    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
-    RECAPTCHA_OPTIONS = os.environ.get('RECAPTCHA_OPTIONS')
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+SQLALCHEMY_TRACK_MODIFICATION = False
